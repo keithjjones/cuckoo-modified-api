d20ae18f051f0a9d9a2239b736d18a8722b5fe9e
Keith J. Jones <keith@keithjjones.com>
2016-09-25 12:52:48 -0400

    Added stubs.

---
+++ cuckoo-modified-api.py
@@ -0,0 +1,29 @@
+#
+# Imports
+#
+
+
+#
+# The Main Function
+#
+def main():
+    """
+    Main function for this library
+    """
+    pass
+
+
+#
+# API Stubs
+#
+def filecreate():
+    """
+    Add a file to Cuckoo for analysis.
+    """
+    pass
+
+#
+# Call main if run as a script
+#
+if __name__ == '__main__':
+    main()
---
+++ libs/Cuckoo.py
@@ -0,0 +1,15 @@
+#
+# INCLUDES
+#
+
+
+#
+# Classes
+#
+class Cuckoo(object):
+    """
+    Class to hold Cuckoo API data.
+    """
+    def __init(self, host="127.0.0.1", port="8000"):
+        self.host = host
+        self.port = port
