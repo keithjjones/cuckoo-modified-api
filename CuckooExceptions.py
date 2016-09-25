692faed7258753c44716e6fe00a4c6be348c3f5f
Keith J. Jones <keith@keithjjones.com>
2016-09-25 17:27:36 -0400

    Added new exception types.

--- CuckooExceptions.py
+++ CuckooExceptions.py
@@ -12,3 +12,25 @@ class CuckooInvalidFileException(Exception):
     """
     def __init__(self, filepath):
         Exception.__init__(self, "Cuckoo: Invalid File {0}".format(filepath))
+
+
+class CuckooAPINotImplemented(Exception):
+    """
+    Exception for when a call is not implemented, but is available.
+    """
+    def __init__(self, apicall):
+        Exception.__init__(self,
+                           "CuckooAPI: Not Implemented {}".format(apicall))
+
+
+class CuckooAPINotAvailable(Exception):
+    """
+    Exception for when a call is not available on the remote server.
+    This signifies you may have used an API call meant for the Django
+    interface and sent it to the api.py interface, or vice versa.
+    """
+    def __init__(self, apicall):
+        Exception.__init__(self, "CuckooAPI: This API is not available for "
+                                 "your target Cuckoo server.  Are you mixing "
+                                 "calls from Django web interface with the "
+                                 "api.py interface?  Or the other way around?")
