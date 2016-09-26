fd5aecf0b1855487ef2460c946f489db48016e75
Keith Jones <kjones@cylance.com>
2016-09-26 12:38:15 -0400

    Added setup files.

--- setup.py
+++ setup.py
@@ -1,26 +1,17 @@
 from distutils.core import setup
-from pip.req import parse_requirements
-from setuptools import find_packages
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt")
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='cuckoo-modified-api',
     version='20160926.1',
     author='Keith J. Jones',
     author_email='keith@keithjjones.com',
-    packages=find_packages(),
+    packages=['CuckooAPI'],
     url='https://github.com/keithjjones/cuckoo-modified-api',
     license='LICENSE',
     description=('A Python library to '
                  'interface with a cuckoo-modified instance'),
     long_description=open('README.MD').read(),
-    install_requires=reqs,
+    install_requires=["requests[security]"],
     entry_points={
         'console_scripts': [
             'CuckooAPI = CuckooAPI.__main__:main'
